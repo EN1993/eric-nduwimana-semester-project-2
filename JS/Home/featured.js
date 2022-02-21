1179b1422dad6312cf191c5b71c2289659302895
EN1993 <74547465+EN1993@users.noreply.github.com>
2022-02-21 16:05:27 +0100

    add

---
+++ JS/Home/featured.js
@@ -0,0 +1,22 @@
+const baseUrl ='http://localhost:1337';
+const featuredContainer = document.querySelector(".featured");
+
+async function featuredPosts(url) {
+  // const fullyQualifiedUrl = `${url}/uploads/jakob_owens_Jz_J_Syb_P_Fb3s_unsplash_0840f6de0c.jpg`
+  // const response = await fetch(fullyQualifiedUrl);
+  // console.log("Posts: ", response);
+  // const posts = await response.json();
+
+  const productsUrl = `${url}/products`
+  const productsResponse = await fetch(productsUrl);
+  const resolvedResponse = await productsResponse.json() 
+  console.log(resolvedResponse);
+
+ featuredContainer.innerHTML += `
+   <div class='featured-picture' style="background-image:url(${resolvedResponse[0].image})"></div>
+   <h3>${resolvedResponse.title}</h3>
+   <p></p>
+  `
+  
+}
+featuredPosts(baseUrl);
--- JS/Home/heroBanner.js
+++ JS/Home/heroBanner.js
@@ -1,13 +1,13 @@
-const baseUrl ='http://localhost:1337/';
+const baseUrl ='http://localhost:1337';
 const heroBannerContainer = document.querySelector(".hero-banner");
 
 async function heroBannerPost(url) {
-  const response = await fetch(url);
-  const posts = await response.json();
-  console.log(posts);
-
+  const fullyQualifiedUrl = `${url}/uploads/jakob_owens_Jz_J_Syb_P_Fb3s_unsplash_0840f6de0c.jpg`
+  const response = await fetch(fullyQualifiedUrl);
+  // console.log( response);
+  // const posts = await response.json();
   heroBannerContainer.innerHTML += `
-  <div class='hero-banner-picture' style="background-image:url('${posts.hero_banner.url}')"></div>
+  <div class='hero-banner-picture' style="background-image:url('${response.url}')"></div>
   `
   
 }
