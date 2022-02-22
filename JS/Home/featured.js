24dd36569e88ddb1b0542bc497298b178a2c9914
EN1993 <74547465+EN1993@users.noreply.github.com>
2022-02-22 19:05:44 +0100

    add

--- JS/Home/featured.js
+++ JS/Home/featured.js
@@ -2,21 +2,28 @@ const baseUrl ='http://localhost:1337';
 const featuredContainer = document.querySelector(".featured");
 
 async function featuredPosts(url) {
-  // const fullyQualifiedUrl = `${url}/uploads/jakob_owens_Jz_J_Syb_P_Fb3s_unsplash_0840f6de0c.jpg`
-  // const response = await fetch(fullyQualifiedUrl);
-  // console.log("Posts: ", response);
-  // const posts = await response.json();
-
+ 
   const productsUrl = `${url}/products`
   const productsResponse = await fetch(productsUrl);
   const resolvedResponse = await productsResponse.json() 
   console.log(resolvedResponse);
 
- featuredContainer.innerHTML += `
-   <div class='featured-picture' style="background-image:url(${resolvedResponse[0].image})"></div>
-   <h3>${resolvedResponse[0].title}</h3>
-   <p></p>
-  `
+ 
+
+  resolvedResponse.map(function (currentItem) {
+
+
+    featuredContainer.innerHTML += ` <div class="featured-content">
+                                          <div class='featured-picture' style="background-image:url(${baseUrl}${currentItem.image.url})"></div>
+                                          <figcaption><h3>${currentItem.title}</h3></figcaption>
+                                          
   
+  
+                                     </div>`
+    
+    })
+   
+
+ 
 }
 featuredPosts(baseUrl);
