5964d9be6a4b4a11e4c587b8c55fdde47677277c
EN1993 <74547465+EN1993@users.noreply.github.com>
2022-02-23 15:55:25 +0100

    Update detail.js

--- JS/shop/detail.js
+++ JS/shop/detail.js
@@ -2,13 +2,13 @@ const querystring = document.location.search;
 const parms = new URLSearchParams(querystring);
 const id = parms.get("id");
 
-const baseUrl ='http://localhost:1337' +id;
+const baseUrl ='http://localhost:1337';
 
 const detailContainer = document.querySelector(".detail");
 
 async function shopDetail(url) {
  
-  const productsUrl = `${url}/products `
+  const productsUrl = `${url}/products/ ` +id;
   const productsResponse = await fetch(productsUrl);
   const resolvedResponse = await productsResponse.json() 
   console.log(resolvedResponse);
