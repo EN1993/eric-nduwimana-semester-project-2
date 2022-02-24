f8be42c0015bae07ff97b90b5c4860b92bf93ef1
EN1993 <74547465+EN1993@users.noreply.github.com>
2022-02-24 15:50:28 +0100

    add

--- JS/shop/detail.js
+++ JS/shop/detail.js
@@ -7,26 +7,36 @@ const baseUrl ='http://localhost:1337';
 const detailContainer = document.querySelector(".detail");
 
 async function shopDetail(url) {
- 
-  const productsUrl = `${url}/products/ ` +id;
+
+
+  // JS Reads code top to bottom, left to right
+  // back ticks are a new feature for string in javascript
+  // Purpose it to make writing multiline strings easy and readable
+  const string1 = "something"
+  const string2 = 'something'
+  const string = `something`
+
+  
+  const productsUrl = `${url}/products/${id}`;
   const productsResponse = await fetch(productsUrl);
   const resolvedResponse = await productsResponse.json() 
   console.log(resolvedResponse);
 
- 
 
-  resolvedResponse.map(function (currentItem) {
-
-
-    detailContainer.innerHTML += ` <div>
-                                    <div class='featured-picture' style="background-image:url(${baseUrl}${currentItem.image.url})"></div>
-                                    <h4>${currentItem.title}</h4>
-                                    <p class="shop-price">Price: $${currentItem.price}</p>
-                                          
+    detailContainer.innerHTML += ` <div  class=" detail--content">
+                                    <div class='detail-picture' style="background-image:url(${baseUrl}${resolvedResponse.image.url})"></div>
+                                   
+                                    <div class=" detail-content"> 
+                                      <h4>${resolvedResponse.title}</h4>
+                                      <p class="shop-price">Price: $${resolvedResponse.price}</p>
+                                      <p class="detail-description"><b> Description:</b> <br>
+                                        ${resolvedResponse.description}
+                                      </p>
+                                      <button class="cta detail-btn"> Add To Cart </button> 
+                                    </div> 
                                   </div>`
     
-    })
-   
+ 
 
  
 }
